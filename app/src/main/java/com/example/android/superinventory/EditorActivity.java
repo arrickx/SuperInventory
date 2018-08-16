ce59d6021ff994637bd4b9a141403ff10eb5df5e
arrickx <24638167+arrickx@users.noreply.github.com>
2018-08-15 21:07:32 -0400

    1. update strings.xml 2. update EditorActivity to use cursorAdapter and able to update the inventory

--- app/src/main/java/com/example/android/superinventory/EditorActivity.java
+++ app/src/main/java/com/example/android/superinventory/EditorActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.superinventory;
 
 import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
@@ -9,18 +12,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.content.Loader;
 import android.app.LoaderManager;
 import com.example.android.superinventory.data.InventoryContract.InventoryEntry;
-
+import android.app.AlertDialog;
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 //Create new inventory or edit the existing inventory
-public class EditorActivity extends AppCompatActivity {
+public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     //Bind view with Butter Knife for cleaner code.
     @BindView(R.id.edit_product_name) EditText mProductNameEditText;
@@ -31,11 +36,28 @@ public class EditorActivity extends AppCompatActivity {
     @BindString(R.string.warning_message) String warningMessage;
     @BindString(R.string.error_message) String errorMessage;
     @BindString(R.string.success_message) String successMessage;
+    @BindString(R.string.error_message_update) String errorUpdate;
+    @BindString(R.string.success_message_update) String successUpdate;
     @BindString(R.string.add_an_inventory) String addInventory;
     @BindString(R.string.edit_an_inventory) String editInventory;
+    @BindString(R.string.empty_string) String nothing;
 
     // Identifier for the inventory data loader
     private static final int EXISTING_INVENTORY_LOADER = 0;
+
+    // Content URI for the existing inventory
+    private Uri mCurrentUri;
+
+    // Boolean flag that keeps track of whether the item has been edited (true) or not (false)
+    private boolean mHasChanged = false;
+
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            mHasChanged = true;
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +67,7 @@ public class EditorActivity extends AppCompatActivity {
 
         // Examine the intent that was used to launch this activity,
         Intent intent = getIntent();
-        Uri mCurrentUri = intent.getData();
+        mCurrentUri = intent.getData();
 
         // If the intent DOES NOT contain a inventory content URI, then we know that we are
         // creating a new inventory.
@@ -56,11 +78,22 @@ public class EditorActivity extends AppCompatActivity {
         } else {
             // Otherwise this is an existing inventory, so change app bar to say "Edit inventory"
             setTitle(editInventory);
+
+            // Initialize a loader to read the inventory data from the database
+            // and display the current values in the editor
+            getLoaderManager().initLoader(EXISTING_INVENTORY_LOADER, null, this);
         }
+
+        // Check if it's edited or not.
+        mProductNameEditText.setOnTouchListener(mTouchListener);
+        mProductPriceEditText.setOnTouchListener(mTouchListener);
+        mProductQuantityEditText.setOnTouchListener(mTouchListener);
+        mSupplierNameEditText.setOnTouchListener(mTouchListener);
+        mSupplierPhoneEditText.setOnTouchListener(mTouchListener);
     }
 
-    //Get user input from editor and save new inventory into database.
-    private void insertInventory() {
+    //Get user input from editor and save inventory into database.
+    private void saveInventory() {
         String productNameString = mProductNameEditText.getText().toString().trim();
         String productPriceString = mProductPriceEditText.getText().toString().trim();
         String productQuantityString = mProductQuantityEditText.getText().toString().trim();
@@ -76,18 +109,34 @@ public class EditorActivity extends AppCompatActivity {
         values.put(InventoryEntry.COLUMN_SUPPLIER_NAME, supplierNameString);
         values.put(InventoryEntry.COLUMN_SUPPLIER_PHONE, supplierPhoneString);
 
-        // Insert a new row of inventory into the provider using the ContentResolver.
-        Uri newUri = getContentResolver().insert(InventoryEntry.CONTENT_URI, values);
-
-        // Show a toast message depending on whether or not the insertion was successful
-        if (newUri == null) {
-            // If the row ID is -1, then there was an error with insertion.
-            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+        // Determine if this is a new or existing inventory by checking if mCurrentUri is null or not
+        if (mCurrentUri == null) {
+            // This is a NEW inventory, so insert a new inventory into the provider,
+            // returning the content URI for the new inventory.
+            Uri newUri = getContentResolver().insert(InventoryEntry.CONTENT_URI, values);
+            // Show a toast message depending on whether or not the insertion was successful.
+            if (newUri == null) {
+                // If the new content URI is null, then there was an error with insertion.
+                Toast.makeText(this, successMessage, Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the insertion was successful and we can display a toast.
+                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+            }
         } else {
-            // Otherwise, the insertion was successful and we can display a toast with the row ID.
-            Toast.makeText(this, successMessage , Toast.LENGTH_SHORT).show();
+                int rowsAffected = getContentResolver().update(mCurrentUri, values, null, null);
+
+                // Show a toast message depending on whether or not the update was successful.
+                if (rowsAffected == 0) {
+                    // If no rows were affected, then there was an error with the update.
+                    Toast.makeText(this, errorUpdate,
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    // Otherwise, the update was successful and we can display a toast.
+                    Toast.makeText(this, successUpdate,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
         }
-    }
 
     //Check if EditText is empty or not
     boolean isEmpty(EditText text) {
@@ -117,7 +166,7 @@ public class EditorActivity extends AppCompatActivity {
             }
             return false;
         } else {
-            insertInventory();
+            saveInventory();
             finish();
             return true;
         }
@@ -144,10 +193,140 @@ public class EditorActivity extends AppCompatActivity {
                 return true;
             // Respond to a click on the "Up" arrow button in the app bar
             case android.R.id.home:
-                // Navigate back to parent activity (CatalogActivity)
-                NavUtils.navigateUpFromSameTask(this);
+                // If the pet hasn't changed, continue with navigating up to parent activity
+                // which is the {@link CatalogActivity}.
+                if (!mHasChanged) {
+                    NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                    return true;
+                }
+                // Otherwise if there are unsaved changes, setup a dialog to warn the user.
+                // Create a click listener to handle the user confirming that
+                // changes should be discarded.
+                DialogInterface.OnClickListener discardButtonClickListener =
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // User clicked "Discard" button, navigate to parent activity.
+                                NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                            }
+                        };
+                // Show a dialog that notifies the user they have unsaved changes
+                showUnsavedChangesDialog(discardButtonClickListener);
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * This method is called when the back button is pressed.
+     */
+    @Override
+    public void onBackPressed() {
+        // If the pet hasn't changed, continue with handling back button press
+        if (!mHasChanged) {
+            super.onBackPressed();
+            return;
+        }
+        // Otherwise if there are unsaved changes, setup a dialog to warn the user.
+        // Create a click listener to handle the user confirming that changes should be discarded.
+        DialogInterface.OnClickListener discardButtonClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // User clicked "Discard" button, close the current activity.
+                        finish();
+                    }
+                };
+        // Show dialog that there are unsaved changes
+        showUnsavedChangesDialog(discardButtonClickListener);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        String[] projection = {
+                InventoryEntry._ID,
+                InventoryEntry.COLUMN_PRODUCT_NAME,
+                InventoryEntry.COLUMN_PRODUCT_PRICE,
+                InventoryEntry.COLUMN_PRODUCT_QUANTITY,
+                InventoryEntry.COLUMN_SUPPLIER_NAME,
+                InventoryEntry.COLUMN_SUPPLIER_PHONE};
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                InventoryEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Bail early if the cursor is null or there is less than 1 row in the cursor
+        if (cursor == null || cursor.getCount() < 1) {
+            return;
+        }
+        // Proceed with moving to the first row of the cursor and reading data from it
+        // (This should be the only row in the cursor)
+        if (cursor.moveToFirst()) {
+            // Find the columns of inventory attributes
+            int productNameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_NAME);
+            int productPriceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_PRICE);
+            int productQuantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_QUANTITY);
+            int supplierNameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_SUPPLIER_NAME);
+            int supplierPhoneColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_SUPPLIER_PHONE);
+
+            // Extract out the value from the Cursor for the given column index
+            String productName = cursor.getString(productNameColumnIndex);
+            double productPrice = cursor.getDouble(productPriceColumnIndex);
+            int productQuantity = cursor.getInt(productQuantityColumnIndex);
+            String supplierName = cursor.getString(supplierNameColumnIndex);
+            String supplierPhone = cursor.getString(supplierPhoneColumnIndex);
+
+            // Update the views on the screen with the values from the database
+            mProductNameEditText.setText(productName);
+            mProductPriceEditText.setText(Double.toString(productPrice));
+            mProductQuantityEditText.setText(Integer.toString(productQuantity));
+            mSupplierNameEditText.setText(supplierName);
+            mSupplierPhoneEditText.setText(supplierPhone);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // If the loader is invalidated, clear out all the data from the input fields.
+        mProductNameEditText.setText(nothing);
+        mProductPriceEditText.setText(nothing);
+        mProductQuantityEditText.setText(nothing);
+        mSupplierNameEditText.setText(nothing);
+        mSupplierPhoneEditText.setText(nothing);
+    }
+
+    /**
+     * Show a dialog that warns the user there are unsaved changes that will be lost
+     * if they continue leaving the editor.
+     *
+     * @param discardButtonClickListener is the click listener for what to do when
+     *                                   the user confirms they want to discard their changes
+     */
+    private void showUnsavedChangesDialog(
+            DialogInterface.OnClickListener discardButtonClickListener) {
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the positive and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.unsaved_changes_dialog_msg);
+        builder.setPositiveButton(R.string.discard, discardButtonClickListener);
+        builder.setNegativeButton(R.string.keep_editing, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Keep editing" button, so dismiss the dialog
+                // and continue editing the pet.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
